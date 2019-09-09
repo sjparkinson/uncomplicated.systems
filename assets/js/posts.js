711d1025c0132cf3bde32f2b75c47c1b8fe4fdc7
Samuel Parkinson <51677+sjparkinson@users.noreply.github.com>
2019-09-09 09:10:37 +0100

    refactor: remove JS based heading anchors

--- assets/js/posts.js
+++
@@ -1,7 +0,0 @@
-// Append an anchor link to each article header.
-document.querySelectorAll('article h3, article h4, article h5, article h6')
-    .forEach(header => {
-        if (header.id) {
-            header.innerHTML = `${header.innerHTML} <a href="#${header.id}">#</a>`
-        }
-    });
